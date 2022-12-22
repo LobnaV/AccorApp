79cce4418e2b852d449ac6bc68d1809e3b15c7ab
Mohamed Semlali <msemlali.externe@m6.fr>
2022-12-22 18:05:33 +0100

    Feat : first version working on tomcat

--- backend/src/main/java/com/viggo/accor/AccorApplication.java
+++ backend/src/main/java/com/viggo/accor/AccorApplication.java
@@ -2,12 +2,19 @@ package com.viggo.accor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AccorApplication{
+public class AccorApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccorApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AccorApplication.class);
 	}
 
 }
--- backend/src/main/java/com/viggo/accor/controller/UserController.java
+++ backend/src/main/java/com/viggo/accor/controller/UserController.java
@@ -4,7 +4,6 @@ import com.viggo.accor.model.User;
 import com.viggo.accor.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
